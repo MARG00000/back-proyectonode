c90ef070de158a5c6c8da728f53965d01b0614aa
MARG00000 <612010579@utsalamanca.edu.mx>
2022-10-19 12:37:35 -0500

    pusimos muchas cosas xd

--- index.js
+++ index.js
@@ -11,7 +11,30 @@ app.use(bodyparser.urlencoded({
     extended: false
 
 }))
+
 app.use(bodyparser.json())
+//conexion de base de datos
+const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.i8bofrs.mongodb.net/${process.env.DBNAME}`
+mongoose.connect(uri, {
+
+    useNewUrlParser: true,
+
+    useUnifiedTopology: true
+
+}).then(() => {
+
+    console.log('conectao a db')
+
+}).catch(e => {
+
+    console.log('error: ', e)
+
+})
+//importar rutas
+const authRoutes = require('./routes/auth.js')
+
+
+app.use('./api/user', authRoutes)
 
 app.get('/',(req , res) =>{
     res.json({
@@ -25,3 +48,4 @@ app.listen(PORT , () =>{
     console.log('Servidor jalando,excelente' + (PORT))
 
 })
+
